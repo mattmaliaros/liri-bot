429caf8de1ca04304874922df9e4e26edbbad616
Matt <mattmaliaros@gmail.com>
2019-07-25 21:39:31 -0400

    working solution

--- liri.js
+++ liri.js
@@ -6,6 +6,9 @@ var spotify = new spotifyReq(keys.spotify);
 var moment = require("moment");
 var term = process.argv[2];
 var query = process.argv.splice(3).join(" ");
+var fs = require('fs');
+checkTerm();
+function checkTerm() {
 switch(term) 
 {
     case "concert-this":
@@ -17,18 +20,27 @@ switch(term)
     case "movie-this":
         movieThis();
         break;
+    case "do-what-it-says":
+        doThis();
+        break;
+}
 }
 function concertSearch() {
+    if (!query)
+    {
+        query = "Luciano";
+    }
     var bandURL = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
     axios.get(bandURL).then(function(response) {
         var jsonData = response.data;
     var showData = [
-        "Artist: " + query,
+        "\nArtist: " + query,
         "Venue: " + jsonData[0].venue.name,
         "Location: " + jsonData[0].venue.city,
         "Date: " + moment(jsonData[0].datetime).format("MM/DD/YYYY")
     ].join("\n\n");
     console.log(showData);
+    appendFile(showData);
     });
 }
 function spotifySong()
@@ -43,17 +55,54 @@ function spotifySong()
           }
           var jsonData = data[0];
          var showData = [
-         "Artist(s): " + data.tracks.items[0].artists[0].name,
+         "\nArtist(s): " + data.tracks.items[0].artists[0].name,
          "Song Name: " + data.tracks.items[0].name,
          "Preview Link: " + data.tracks.items[0].preview_url,
          "Album: " + data.tracks.items[0].album.name
          
           ].join("\n\n");
           console.log(showData);
+          appendFile(showData);
+          
     });
 }
 function movieThis() {
+    if (!query)
+    {
+        query = "Waiting";
+    }
     var queryURL = "https://www.omdbapi.com/?t=" + query + "&plot=full&apikey=trilogy";
     axios.get(queryURL).then(function(response) {
+        var jsonData = response.data;
+        var showData = [
+            "\nTitle: " + jsonData.Title,
+            "Year: " + jsonData.Year,
+            "IMDB Rating: " + jsonData.imdbRating,
+            // "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value
+            //This rating isn't always available:  If it isn't, it creates an unhandled promise rejection
+            "Country: " + jsonData.Country,
+            "Language: " + jsonData.Language,
+            "Plot: " + jsonData.Plot,
+            "Actors: " + jsonData.Actors
+        ].join("\n\n");
+        console.log(showData);
+        appendFile(showData);
+        });
+}
+function doThis() {
+fs.readFile('./random.txt', function (err, data) {   
+    if (err) throw err;   
+    var content = data.toString();
+    var split = content.split(",");
+    term = split[0];
+    query = split[1];
+    checkTerm();
+  });
+}
+function appendFile(data){
+var div = "---------------------------------------------------------------------------------------";
+fs.appendFileSync('./log.txt', data + "\n" + div, function (err)
+{
+if (err) throw err;
 })
 }
